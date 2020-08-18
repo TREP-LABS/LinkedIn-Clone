054e98f37eb81dc02a2a1fdde4be09361101194d
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-08-18 02:05:56 +0100

    Add register user controller

---
+++ src/api/controllers/users/users.js
@@ -0,0 +1,14 @@
+import { asyncHandler } from '../../middlewares';
+import { UserService } from '../../services';
+import { successResponse } from '../../utils/helpers';
+
+/**
+ * Registers a new user.
+ * @param {Function} controller The controller function.
+ * @returns {Object} The response object containing some response data.
+ */
+export const register = asyncHandler(async (req, res) => {
+  const token = await UserService.register(req.body);
+
+  return successResponse(res, 'User registered successfully.', { data: { token } }, 201);
+});
