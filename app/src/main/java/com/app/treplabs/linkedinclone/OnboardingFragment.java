15528dae6ce6943edf3bf2666d9fdcc668d6264b
kingahmedino <mohammedoluwasesan@gmail.com>
2020-08-18 22:50:45 +0100

    Added sign up screen

--- app/src/main/java/com/app/treplabs/linkedinclone/OnboardingFragment.java
+++ app/src/main/java/com/app/treplabs/linkedinclone/OnboardingFragment.java
@@ -2,27 +2,36 @@ package com.app.treplabs.linkedinclone;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class OnboardingFragment extends Fragment {
+    private Button mJoinButton;
 
     public OnboardingFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding, container, false);
+        View view = inflater.inflate(R.layout.fragment_onboarding, container, false);
+        mJoinButton = view.findViewById(R.id.join_now_btn);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mJoinButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_onboardingFragment_to_signUpFragment)
+        );
     }
 }
