146d682eec4934fbd12b08d08aa418555dda4e01
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-08-20 11:48:36 +0100

    Setup swagger to autogenerate API docs from JSDoc comment

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,7 +1,41 @@
 import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import authRoutes from './auth.route';
 
 const router = express.Router();
+
+// Setup Swagger for API documentation.
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Linkedin Clone',
+      version: '1.0.0',
+      description: 'API documentation for the LinkedIn Clone App',
+      license: {
+        name: 'MIT',
+        url: 'https://choosealicense.com/licenses/mit/',
+      },
+      contact: {
+        name: 'TREP Labs',
+        url: 'https://treplabs.co',
+        email: 'Info@treplabs.co',
+      },
+    },
+    servers: [
+      {
+        url: 'http://127.0.0.1:5000/api/v1',
+      },
+    ],
+  },
+  apis: ['./src/api/models/*.js', './src/api/routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+router.use('/api-docs', swaggerUi.serve);
+
+router.get('/api-docs', swaggerUi.setup(specs, { explorer: true }));
 
 router.use('/auth', authRoutes);
 
