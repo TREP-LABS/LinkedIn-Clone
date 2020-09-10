6ffb578132fa52adbe189e982ecf41feb5d87a5b
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-09-10 21:27:16 +0100

    Add text index for name field in Skill model

--- src/api/models/Skill.js
+++ src/api/models/Skill.js
@@ -6,4 +6,6 @@ const SkillSchema = new Schema({
   name: { type: String, required: true, unique: true },
 });
 
+SkillSchema.index({ name: 'text' });
+
 export default mongoose.model('Skill', SkillSchema);
