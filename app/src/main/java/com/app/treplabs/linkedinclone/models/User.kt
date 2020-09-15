5603d21f1c5ee5e561c5e3dbbd0936e0b958d62f
kingahmedino <ahmedino001>
2020-09-15 17:02:40 +0100

    Added kotlin support and changed User model to kotlin singleton

---
+++ app/src/main/java/com/app/treplabs/linkedinclone/models/User.kt
@@ -0,0 +1,10 @@
+package com.app.treplabs.linkedinclone.models
+
+object User {
+    var firstName: String? = null
+    var lastName: String? = null
+    var email: String? = null
+    var userId: String? = null
+    var slug: String? = null
+    var token: String? = null
+}
