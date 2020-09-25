3594116a102161fcf1e25656f44e8f5af775b980
kingahmedino <ahmedino001>
2020-09-25 07:30:56 +0100

    Added user certification model

---
+++ app/src/main/java/com/app/treplabs/linkedinclone/models/UserCertificate.kt
@@ -0,0 +1,5 @@
+package com.app.treplabs.linkedinclone.models
+
+class UserCertificate(var name: String, var authority: String, var number: String, var startDate: String,
+                      var endDate: String, var certificateId: String, var url: String, var isPresent: Boolean) {
+}
